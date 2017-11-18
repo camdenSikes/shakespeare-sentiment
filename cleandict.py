ec85126728fd5fb85070aee04dd6d14d66108e0a
camdenSikes <gcdashsj@gmail.com>
2017-11-18 14:26:52 -0600

    added additional texts

--- cleandict.py
+++ cleandict.py
@@ -1,6 +1,9 @@
 """Takes in a dictionary as a text file and turns it into a csv"""
 import csv
 
-infile = open("dict.txt", r)
-outfile = open("dict.csv", w)
+infile = open("dictionary.txt", r)
+outfile = open("dictionary.csv", w)
 dictwriter = csv.writer(outfile, delimiter = "\t")
+
+for line in infile.readlines:
+    
